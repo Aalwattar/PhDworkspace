5912e74aa5e1e1ea5ffa3f9cec6c8a108e86d9e8
Ratchette <jwiner@uoguelph.ca>
2013-05-07 11:28:01 -0400

    Adding modularity to files

---
+++ include/population.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   population.h
+ * Author: Jennifer
+ *
+ * Created on May 7, 2013, 11:23 AM
+ */
+
+#ifndef POPULATION_H
+#define	POPULATION_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* POPULATION_H */
+
