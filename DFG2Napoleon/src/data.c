1f1332b23d99c7301604f25be08587e2c3361295
Ahmed Al-Wattar <aalwatta@uoguelphca>
2013-05-23 15:42:40 -0400

    Stupid simple renaming thing

---
+++ DFG2Napoleon/src/data.c
@@ -0,0 +1,19 @@
+/*
+ * data.c
+ *
+ *  Created on: Jun 1, 2011
+ *      Author: ahmed
+ */
+
+
+#include "data.h"
+/*FIXME */
+//static struct nodeData TasksTable[MAX_NO_OF_NODES];
+struct TaskType TasksTypes[MAX_TASKS_TYPES];
+
+
+struct node *dfg1;
+//struct node *DFGArray[];
+
+
+
