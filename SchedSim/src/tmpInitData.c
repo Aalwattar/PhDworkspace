7799c1ca9eef84cbf7fe5eec64c25b1c3004656c
Ahmed Al-Wattar <aalwatta@uoguelph.ca>
2013-06-18 17:06:42 -0400

    Added two extra files

---
+++ SchedSim/src/tmpInitData.c
@@ -0,0 +1,18 @@
+/*
+ * TempInitData.c
+ *
+ *  Created on: Jun 18, 2013
+ *      Author: aalwatta
+ */
+#include "PlatformConfig.h"
+#include "tmpInitData.h"
+
+unsigned int PRRConfigValues [][50]={
+		{ PRR_0_CONFIG_TIME,  PRR_1_CONFIG_TIME,
+				PRR_2_CONFIG_TIME, PRR_3_CONFIG_TIME, PRR_4_CONFIG_TIME },
+		{5,10,20,30,40},
+		{40,40},
+		{10,10,15,15,20,20},
+		{10,10,15,15,20,20}
+};
+
