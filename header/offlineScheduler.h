f9ed287b9c9ebf5ad8a3ea8626aeda9e6a8f8d39
Ratchette <jenn.winer@gmail.com>
2013-07-14 11:21:51 -0400

    Modified the header files

--- header/offlineScheduler.h
+++ header/offlineScheduler.h
@@ -25,25 +25,22 @@
 #ifndef RCSSIMULATOR_H_
 #define RCSSIMULATOR_H_
 
-struct SimData {
-    int dFGID;
-    int noPRR;
-    int noGPP;
+struct SimData{
+    unsigned int flags;
     int noOfNodes;
     int typeData[500];
 };
 
-
-struct SimResults {
-    int totalTime;
-    int noOfReuse;
-    int noOfConfiguration;
-    int noSW2HWMigration;
-    int noHW2SWMigration;
-    int noOfSWTasks;
-    int noSWBusyCounter;
-    int noHWBusyCounter;
-    int power;
+struct SimResults{
+    unsigned int totalTime;
+    unsigned int noOfReuse;
+    unsigned int noOfConfiguration;
+    unsigned int noSW2HWMigration;
+    unsigned int noHW2SWMigration;
+    unsigned int noOfSWTasks;
+    unsigned int noSWBusyCounter;
+    unsigned int noHWBusyCounter;
+    unsigned int power;
 };
 
 #endif // RCSSIMULATOR_H_
